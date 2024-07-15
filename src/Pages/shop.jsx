c10dfaa4af08bb2f051d52731b6cf6efe7842cdd
sivarjun28 <arjunyadav6407@gmail.com>
2024-07-15 22:14:12 +0530

    New Collection-News Letter-Footer created

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const shop = () => {
   return (
@@ -9,6 +11,8 @@ const shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
